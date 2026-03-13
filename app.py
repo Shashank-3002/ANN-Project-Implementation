255b460b8a58574bebc4a68393e9a5a7aa7d6c9d
Shashank Naragundmath <shashanknaragundmath@gmail.com>
2026-03-13 21:45:46 +0530

    Refactor imports and improve prediction output

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+from keras.models import load_model
+from sklearn. preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pickle
 
 ##Load the trained ANN model
